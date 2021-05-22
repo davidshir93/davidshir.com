f48257d3677b894bbbcb9a66474ee91a9aa55ee2
davidshir93 <davidshir93@gmail.com>
2021-05-22 18:43:12 +0300

    wip 22.5.21

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,8 +1,42 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+        x.className += " responsive";
     } else {
-      x.className = "topnav";
+        x.className = "topnav";
     }
-  }
+}
+
+// var intervalID = scope.setInterval(document.title = Math.random(), [2]);
+
+// testing scripts
+
+// let el = document.querySelectorAll('#item');
+// let px = document.getElementById('mx');
+// let py = document.getElementById('my');
+// console.log(el);
+// window.addEventListener('mousemove', showPosition, false);
+
+// el.forEach(addEventListener('click', function(e) {
+//     itemDone(e);
+// }, false));
+
+// function itemDone(e) {
+//     console.log('itemDone function triggered')
+//         // remove the item from the list
+//     let target = e.target;
+//     let elParent = e.target.parentNode;
+//     let newMessage = document.createElement('span');
+//     newMessage.innerHTML = e.target.textContent + ' removed! ';
+//     elParent.removeChild(target);
+//     elParent.appendChild(newMessage);
+//     e.preventDefault();
+// };
+
+
+// function showPosition(e) {
+//     px.innerHTML = e.pageX;
+//     py.innerHTML = e.pageY;
+// }
+
+// let body = document.querySelector('body');
