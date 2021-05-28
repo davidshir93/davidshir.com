a3577f0c2f191a8e4ed455d3a381931e8811d24a
David Shir <davidshir93@gmail.com>
2021-05-28 20:11:11 +0300

    great progress

--- portfolio/script.js
+++ portfolio/script.js
@@ -7,6 +7,28 @@ function myFunction() {
     }
 }
 
+var isClicked = false;
+
+var f = document.getElementById('foo');
+document.addEventListener('click', function(ev){
+    if (!isClicked) {
+        f.style.borderRadius = '25px';
+        f.style.transform = 'translateY('+(ev.clientY-25)+'px)';
+        f.style.transform += 'translateX('+(ev.clientX-25)+'px)';
+        isClicked = true;
+    } else {
+        f.style.borderRadius = '0';
+        f.style.transform = 'translateY('+(ev.clientY-25)+'px)';
+        f.style.transform += 'translateX('+(ev.clientX-25)+'px)';
+        f.style.transform += 'scale(2)';
+
+        isClicked = false;
+    }
+    
+    
+},false);
+
+
 // var intervalID = scope.setInterval(document.title = Math.random(), [2]);
 
 // testing scripts
