31a9e7b835704f475b037d56860845038ef70d1c
David Shir <davidshir93@gmail.com>
2021-05-29 17:38:17 +0300

    v3

---
+++ vueTestTwo/trackerapp2/src/components/HeaderMe.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green"></Button>
+  </header>
+</template>
+
+<script lang="ts">
+import Button from "./Button";
+
+export default {
+  name: 'HeaderMe',
+  components: { Button },
+  props: {
+    title: String
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
