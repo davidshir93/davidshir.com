d340f8faf2b66c0d6d27814898c6ba10013304f7
David Shir <davidshir93@gmail.com>
2021-07-27 10:57:17 +0300

    wip 12

--- portfolio/script.js
+++ portfolio/script.js
@@ -6,34 +6,3 @@ console.info(
   "davidshir93@gmail.com"
 );
 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-var isClicked = false;
-
-var f = document.getElementById("foo");
-document.addEventListener(
-  "click",
-  function (ev) {
-    if (!isClicked) {
-      f.style.borderRadius = "25px";
-      f.style.transform = "translateY(" + (ev.clientY - 25) + "px)";
-      f.style.transform += "translateX(" + (ev.clientX - 25) + "px)";
-      isClicked = true;
-    } else {
-      f.style.borderRadius = "0";
-      f.style.transform = "translateY(" + (ev.clientY - 25) + "px)";
-      f.style.transform += "translateX(" + (ev.clientX - 25) + "px)";
-      f.style.transform += "scale(2)";
-
-      isClicked = false;
-    }
-  },
-  false
-);
