3a07b86b036f5e94c8729ceb1b654e0c78e251bb
David Shir <davidshir93@gmail.com>
2021-08-01 16:25:39 +0300

    wip 4

--- IBP-Value-V2/assets/js/script.js
+++ IBP-Value-V2/assets/js/script.js
@@ -73,6 +73,7 @@ for (let i = 0; i < 35; i++) {
     newCircle.style.height = `100px`;
     newCircle.style.width = `100px`;
     newCircle.style.boxShadow = '1px 1px 0px #000000';
+    newCircle.style.textShadow = '4px 3px 0 #000, -1px -1px 0 #000, 1px -1px 0 #000, 1px 1px 0 #000, 2px 2px 0px #000;';
     newCircle.style.transform = `scale(${scaleFactor})`
     newCircle.innerHTML=`<h4 style="text-shadow: 1px 1px 0px #000000; color: #FFFFFF;">${Math.floor(Math.random()*15)}%</h4>`;
     newCircles.push(newCircle);
