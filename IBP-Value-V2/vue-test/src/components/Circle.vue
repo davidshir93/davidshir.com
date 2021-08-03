c45f4e22bcf9c54974c308ea9a18a75b08ee7327
David Shir <davidshir93@gmail.com>
2021-08-03 15:45:27 +0300

    vue test

---
+++ IBP-Value-V2/vue-test/src/components/Circle.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="circle" :style="{backgroundColor: color}">
+    {{precentage}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Circle',
+  props: {
+    precentage: {
+      type: Number,
+      default: 5,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: '#000000',
+      required: true,
+    },
+    position: {
+      type: Object,
+      default: () => ({
+        x: 100,
+        y: 100
+      }),
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
