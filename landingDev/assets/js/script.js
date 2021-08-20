7b19849251d58084f43394445391cab731ac49ec
David Shir <davidshir93@gmail.com>
2021-08-20 20:05:23 +0300

    landing dev wip

--- landingDev/assets/js/script.js
+++ landingDev/assets/js/script.js
@@ -3,18 +3,19 @@ const marker = document.getElementById('marker');
 const nav = document.querySelector('nav');
 
 const options = {
-    // threshold: 0.5,
+    root: document.querySelector('[data-scroll-root]'),
     rootMargin: '0px',
+    threshold: 1.0,
 }
 
-let observer = new IntersectionObserver(navCheck, options);
-
-function navCheck(entries) {
+const navCheck = (entries, observer) => {
     entries.forEach(entry => {
+
         const className = entry.target.className;
         const activeAnchor = document.querySelector(`[data-page=${className}]`)
         const coords = activeAnchor.getBoundingClientRect();
-        console.log(coords)
+        console.log(coords);
+
         let directions = {
             height: coords.height,
             width: coords.width,
@@ -22,15 +23,30 @@ function navCheck(entries) {
             left: coords.left,
             right: coords.right
         };
+
         console.log(directions.width);
+
         if(entry.isIntersecting){
+            marker.style.setProperty('opacity', '1');
             marker.style.setProperty('left', `${directions.left}px`);
             marker.style.setProperty('bottom', `${directions.top}px`);
             marker.style.setProperty('height', `${directions.height}px`);
             marker.style.setProperty('width', `${directions.width}px`);
+            // activeAnchor.classList.add('active');
+        } 
+        
+        if(entry.intersectionRatio < 0.3){
+            activeAnchor.classList.remove('active');
         }
+
+        if(entry.intersectionRatio < 0.1){
+            marker.style.setProperty('opacity', '0');
+        }
+
     })
 }
+
+let observer = new IntersectionObserver(navCheck, options);
 
 sections.forEach(section => {
     observer.observe(section)
