e436cb1be336024634a7fb4ebf3f81bd0fb06a0e
David Shir <davidshir93@gmail.com>
2021-08-28 17:17:54 +0300

    react crash crashed it!!!

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
+import { useLocation } from 'react-router-dom'
 
 const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button text={showAdd ? 'Cancel' : 'Add'} color={showAdd ? 'gray' : 'green'} onClick={onAdd}/>
+            {location.pathname === '/' && <Button text={showAdd ? 'Cancel' : 'Add'} color={showAdd ? 'gray' : 'green'} onClick={onAdd}/>}
         </header>
     )
 }
