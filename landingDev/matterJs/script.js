71903692e719500d5b4a0aa6e4d18bfd31c277a8
David Shir <davidshir93@gmail.com>
2022-04-23 15:16:33 +0300

    1

---
+++ landingDev/matterJs/script.js
@@ -0,0 +1,65 @@
+// console.log('hello world');
+
+// // module aliases
+// var Engine = Matter.Engine,
+// 	Render = Matter.Render,
+// 	Runner = Matter.Runner,
+// 	Bodies = Matter.Bodies,
+// 	Composite = Matter.Composite;
+
+// // create an engine
+// var engine = Engine.create();
+
+// // create a renderer
+// var render = Render.create({
+// 	element: document.body,
+// 	engine: engine,
+// });
+
+// Matter.js module aliases
+var Engine = Matter.Engine,
+	World = Matter.World,
+	Body = Matter.Body,
+	Bodies = Matter.Bodies,
+	Composites = Matter.Composites,
+	Composite = Matter.Composite,
+	Constraint = Matter.Constraint,
+	engine;
+
+(function () {
+	var canvas = document.getElementById('canvas');
+
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	engine = Engine.create({
+		render: {
+			canvas: canvas,
+		},
+	});
+
+	window.addEventListener('resize', function () {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+	});
+
+	// run the engine
+	Engine.run(engine);
+})();
+
+// create two boxes and a ground
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+Composite.add(engine.world, [boxA, boxB, ground]);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+var runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
